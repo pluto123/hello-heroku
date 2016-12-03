004360a832831ae151ef54c2e6027d28b89ae9e3
pluto123 <yuh.chang.wu@gmail.com>
2016-12-03 19:34:50 +0800

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,13 @@
+import unittest
+import web
+
+classs FlaskTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = web.app.test_client()
+        
+    def test_index(self):
+        rv = self.app.get('/')
+        assert b'hello Steven Wu' in rv.data
+        
+if __name__ == '__main__':
+    unittest.main()
